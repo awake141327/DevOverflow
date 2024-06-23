3766083c3c43a738c095e7c27e6bd4c6c0ad6fb9
Amaan <amasume141@gmail.com>
2024-06-24 03:13:27 +0530

    add routing and auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="flex w-full min-h-screen items-center justify-center">
+      {children}
+    </main>
+  );
+}
